50661fcd17c354affdd23099cadf13bcf624be2c
Douglas Leandro <douglaslfernandes@gmail.com>
2014-02-12 15:32:21 -0300

    Exercice 3.5.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
     
-    it "should have the right title" do
+    it "should have the right title 'Home" do
       visit '/static_pages/home'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
@@ -22,7 +22,7 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-    it "should have the right title" do
+    it "should have the right title 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
@@ -35,9 +35,21 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
     
-    it "should have the right title" do
+    it "should have the right title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end    
   end
+  
+  describe "Contact page"
+    
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contatc')
+    end
+    
+    it "should have the right title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end  
 end
